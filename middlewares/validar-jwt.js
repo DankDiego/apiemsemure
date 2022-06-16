1f380d927ffe017dc3284e1c9caa7c794231fb94
DankDiego <49381861+DankDiego@users.noreply.github.com>
2022-06-15 22:05:47 -0500

    First commit

---
+++ middlewares/validar-jwt.js
@@ -0,0 +1,58 @@
+const { response, request } = require('express');
+const jwt = require('jsonwebtoken');
+
+const Usuario = require('../models/usuario');
+
+
+const validarJWT = async( req = request, res = response, next ) => {
+
+    const token = req.header('x-token');
+
+    if ( !token ) {
+        console.log('sin token en la peticion')
+        return res.status(401).json({
+            msg: 'No hay token en la petición'
+        });
+    }
+
+    try {
+        
+        const { uid } = jwt.verify( token, process.env.SECRETORPRIVATEKEY );
+
+        // leer el usuario que corresponde al uid
+        console.log('POSIBLE ERORR -*-*-*-*-*-*-*-*-',uid,'aca id error')
+        const usuario = await Usuario.findById( uid );
+        if( !usuario ) {
+            return res.status(401).json({
+                msg: 'Token no válido - usuario no existe DB'
+            })
+        }
+
+        // Verificar si el uid tiene estado true
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Token no válido - usuario con estado: false'
+            })
+        }
+        
+        
+        req.usuario = usuario;
+        req.uid = uid;
+        next();
+
+    } catch (error) {
+
+        console.log(error);
+        res.status(401).json({
+            msg: 'Token no válido'
+        })
+    }
+
+}
+
+
+
+
+module.exports = {
+    validarJWT
+}
